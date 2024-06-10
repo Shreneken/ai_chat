5c78adefdcc15b2f1145d198e35c09621ea2aa88
Shreyan Mallik <shrnmallik@gmail.com>
2024-06-10 18:39:05 +0545

    fix: added structure

--- chatbot-frontend/src/App.tsx
+++ chatbot-frontend/src/App.tsx
@@ -1,33 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { HomePage } from "./pages/Home";
+import { Secret } from "./pages/Secret";
 
-export default function App() {
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "./hooks/useAuth";
+
+function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/about" element={<About/>} />
-          <Route path="/" element={<Home/>} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/secret"
+          element={
+            <ProtectedRoute>
+              <Secret />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
+export default App;
---
+++ chatbot-frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export function HomePage() {
+  return <h1> THIS IS HOME</h1>;
+}
+
+
